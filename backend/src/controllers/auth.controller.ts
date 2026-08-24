f6151fb7400f6b9e0b1a937b011a654d1909e291
Ali <alimehdizadeh127@gmail.com>
2026-08-24 09:12:33 +0330

    feat: Add login route and implmenet functionlaity. Add loading indicator to healthCheck page

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import jwt, { SignOptions } from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import prisma from "../lib/prisma";
 
@@ -42,6 +43,52 @@ export async function signup(req: Request, res: Response, next: NextFunction) {
       status: "error",
       message: "Something went wrong",
     });
+  }
+}
+
+export async function login(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { email, password } = req.body;
+
+    // check if user exists
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email" });
+    }
+
+    // Compare password
+    const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid password" });
+
+    // Create tokens
+    const accessToken = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_ACCESS_SECRET as string,
+      { expiresIn: process.env.JWT_ACCESS_EXPIRES_IN } as SignOptions,
+    );
+
+    const refreshToken = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_REFRESH_SECRET as string,
+      { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN } as SignOptions,
+    );
+
+    const { passwordHash: _, ...userWithoutPassword } = user;
+
+    return res.status(200).json({
+      status: "ok",
+      data: {
+        user: userWithoutPassword,
+        accessToken,
+        refreshToken,
+      },
+    });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
   }
 }
 
--- frontend/fixhub/src/app/page.tsx
+++ frontend/fixhub/src/app/page.tsx
@@ -3,12 +3,43 @@
 import { getHealth } from "../lib/api";
 import { useEffect, useState } from "react";
 
+type HealthResponse = {
+  status: string;
+  message: string;
+};
+
 export default function Home() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<HealthResponse | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    getHealth().then(setData).catch(console.error);
+    async function fetchHealth() {
+      try {
+        setIsLoading(true);
+        const res = await getHealth();
+        setIsLoading(false);
+        setData(res);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError("Something went wrong");
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchHealth();
   }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div style={{ color: "red" }}>{error}</div>;
+  }
 
   return (
     <div>
